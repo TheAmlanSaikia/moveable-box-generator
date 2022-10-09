9cd4e438ff295673d45af5437c216a340d09251c
TheAmlanSaikia <83667291+TheAmlanSaikia@users.noreply.github.com>
2022-10-09 14:48:04 +0530

    feat: initial commit

---
+++ src/Page/Home/Home.jsx
@@ -0,0 +1,19 @@
+const Home = () =>{
+
+    return (
+      <div>
+       <section>
+         <button>Increase Box</button>
+       </section>
+       <section>
+        <h1> Here the boxes will be shown</h1>
+       </section>
+      </div>
+    )
+
+}
+
+
+export { Home };
+
+
